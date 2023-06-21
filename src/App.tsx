0427f580cc78d93c11b3b7626000ac87276ef3b8
FernandoCM18 <fernando.c.m@live.com>
2023-06-21 12:41:51 -0700

    base project

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <header>
+        Header
+        <nav>Nav</nav>
+      </header>
+
+      <main>
+        Main
+        <aside>Aside</aside>
+        <article>Article1</article>
+        <article>Article2</article>
+      </main>
+
+      <footer>Footer</footer>
+    </>
+  );
+}
+
+export default App;
